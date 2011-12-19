eb7af04bc2ecf704a1eae3e07aeb5ad0e4be532d
Eric BREHAULT <ebrehault@gmail.com>
2011-12-19 18:01:35 +0100

    migrate staticfiles to django 1.3

--- src/GeoNodePy/geonode/urls.py
+++ src/GeoNodePy/geonode/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-from staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from geonode.sitemap import LayerSitemap, MapSitemap
 from geonode.proxy.urls import urlpatterns as proxy_urlpatterns
 
